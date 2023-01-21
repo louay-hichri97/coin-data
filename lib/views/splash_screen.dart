0222168eb6b55c027701fe6732ea78c707fc4cfc
louay.hichri <louay.hichri@hotmail.com>
2023-01-21 15:46:57 +0100

    create UI for Splash Screen

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,16 +1,48 @@
 import 'package:coin_data/views/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:splash_screen_view/SplashScreenView.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
-    return SplashScreenView(
-        navigateRoute: const OnBoardingScreen(),
-        duration: 3000,
-        backgroundColor: Colors.black,
-    );
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: const Color(0XFF1B2540),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.4,
+          ),
+          Center(
+            child: Text(
+              "coinData",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                  fontSize: MediaQuery.of(context).size.height * 0.035),
+            ),
+          ),
+          const Spacer(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.025,
+            width: MediaQuery.of(context).size.height * 0.025,
+            child: const CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+        ],
+      ),
+    ));
   }
 }
